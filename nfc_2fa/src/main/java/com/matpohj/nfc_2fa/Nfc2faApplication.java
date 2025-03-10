62d878b569a4a1bf28d34fc46a34a3877dd5ad73
MatPohj <159891127+MatPohj@users.noreply.github.com>
2025-03-10 11:58:24 +0200

    initial commit

---
+++ nfc_2fa/src/main/java/com/matpohj/nfc_2fa/Nfc2faApplication.java
@@ -0,0 +1,13 @@
+package com.matpohj.nfc_2fa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nfc2faApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nfc2faApplication.class, args);
+	}
+
+}
---
+++ nfc_2fa/src/test/java/com/matpohj/nfc_2fa/Nfc2faApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matpohj.nfc_2fa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nfc2faApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
