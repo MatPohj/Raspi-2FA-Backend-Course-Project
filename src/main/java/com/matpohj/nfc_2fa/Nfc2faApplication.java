6e0b00b2d25c3dd24b21832a3579f05d2fe4b8d0
MatPohj <159891127+MatPohj@users.noreply.github.com>
2025-04-26 22:58:10 +0300

    added easy way to change between dev and prod environments

--- src/main/java/com/matpohj/nfc_2fa/Nfc2faApplication.java
+++ src/main/java/com/matpohj/nfc_2fa/Nfc2faApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Nfc2faApplication {
 
 	public static void main(String[] args) {
+		// Set dev profile by default if no profile is active
+		System.setProperty("spring.profiles.default", "dev");
 		SpringApplication.run(Nfc2faApplication.class, args);
 	}
 
